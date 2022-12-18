ec866db149c14cecbe8e44bc73469012f879cbd0
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2022-12-18 10:54:40 +0000

    added exercise database using external api

--- src/components/WorkoutData.js
+++ src/components/WorkoutData.js
@@ -1,81 +1,100 @@
-// import React, { useState, useEffect } from 'react';
+// import React from 'react';
+// class WorkoutData extends React.Component {
+// 	// constructor
+// 	constructor(props) {
+// 		super(props);
 
-// export default function WorkoutData() {
-// 	const [data, setData] = useState(null);
-// 	const [loading, setLoading] = useState(true);
-// 	const [search, setSearch] = useState('');
-
-// 	const url = `https://wger.de/api/v2/exerciseinfo/`;
-
-// 	useEffect(() => {
-// 		fetch('https://wger.de/api/v2/exerciseinfo/')
-// 			.then((response) => response.json())
-// 			.then((usefulData) => {
-// 				console.log(usefulData.results);
-// 				setLoading(false);
-// 				setData(usefulData.results);
+// 		this.state = {
+// 			items: [],
+// 			DataisLoaded: false,
+// 		};
+// 	}
+// 	// componentDidMount used to execute code
+// 	componentDidMount() {
+// 		fetch( 'https://exercisedb.p.rapidapi.com/exercises', {
+//   	"method": "GET",
+//  	 "headers": {
+// 	'X-RapidAPI-Key': '7f72d4ba00mshf1ff8b96262e395p13878bjsn00b8ac2a4c8f',
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// })
+// 		.then((response) => response.json())
+// 			.then((response) => {
+// 				this.setState({
+// 					items: response,
+// 					DataisLoaded: true,
+// 				})
 // 			})
-// 			.catch((e) => {
-// 				console.error(`An error occurred: ${e}`);
-// 			});
-// 	}, []);
+// 			.catch(err => {console.log(err)})
+// 	}
 
-// 	return (
-// 		<div>
-// 			<h1>Hello Workout Data</h1>
+// 	render() {
+// 		const { DataisLoaded, items } = this.state;
+// 		if (!DataisLoaded)
+// 			return (
+// 				<div>
+// 					<h1>Please wait, content loading ...</h1>
+// 				</div>
+// 			);
+
+// 		return (
 // 			<div>
-// 				{' '}
-// 				{loading && <p>Loading...</p>}
-// 				{!loading && <p>Fetched data</p>}
+// 				<h1>Fetched data from api </h1>
+// 				{items.map((item) => (
+// 					<ol key={item.id}>
+// 						Name: {item.name}
+// 						Description: {item.description}
+// 					</ol>
+// 				))}
 // 			</div>
-// 			<div>
-// 				<label>Search Exercise </label>
-// 				<input type='text' onChange={(e) => setSearch(e.target.value)} />
-// 			</div>
-// 		</div>
-// 	);
+// 		);
+// 	}
 // }
-import React from 'react';
-class WorkoutData extends React.Component {
-	// constructor
-	constructor(props) {
-		super(props);
 
-		this.state = {
-			items: [],
-			DataisLoaded: false,
-		};
-	}
-	// componentDidMount used to execute code
-	componentDidMount() {
-		fetch('https://wger.de/api/v2/exerciseinfo/')
+// export default WorkoutData;
+
+import React, { useState, useEffect } from 'react';
+
+export default function WorkoutData() {
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		fetch('https://exercisedb.p.rapidapi.com/exercises', {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '7f72d4ba00mshf1ff8b96262e395p13878bjsn00b8ac2a4c8f',
+				'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+			},
+		})
 			.then((res) => res.json())
-			.then((json) => {
-				this.setState({
-					items: json.results,
-					DataisLoaded: true,
-				});
-			});
-	}
-
-	render() {
-		const { DataisLoaded, items } = this.state;
-		if (!DataisLoaded)
-			return (
-				<div>
-					<h1>Please wait, content loading ...</h1>
-				</div>
+			.then(
+				(result) => {
+					setIsLoaded(true);
+					setItems(result);
+				},
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
 			);
+	}, []);
 
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <div>Loading ...</div>;
+	} else {
 		return (
-			<div>
-				<h1>Fetched data from api </h1>
+			<ul>
 				{items.map((item) => (
-					<ol key={item.id}>Name: {item.name}</ol>
+					<ol key={item.id}>
+						Name: {item.name}
+						Description: {item.description}
+					</ol>
 				))}
-			</div>
+			</ul>
 		);
 	}
 }
-
-export default WorkoutData;
