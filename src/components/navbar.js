5cc7e1d0e24719cc72fa30ce1b763870748f4cb0
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2022-12-31 10:44:43 +0000

    fixed navbar toggle button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import gymrecordlogo from '../images/gymrecordlogo.png';
 
 // Here, we display our Navbar
 export default function Navbar() {
 	return (
-		<div className='App'>
+		// <div>
+		// 	<nav class='navbar navbar-expand-lg navbar-light bg-light'>
+		// 		<a class='navbar-brand' href='#'>
+		// 			Navbar
+		// 		</a>
+		// 		<button
+		// 			class='navbar-toggler'
+		// 			type='button'
+		// 			data-bs-toggle='collapse'
+		// 			data-bs-target='#navbarNav'
+		// 			aria-controls='navbarNav'
+		// 			aria-expanded='false'
+		// 			aria-label='Toggle navigation'
+		// 		>
+		// 			<span class='navbar-toggler-icon'></span>
+		// 		</button>
+		// 		<div class='collapse navbar-collapse' id='navbarNav'>
+		// 			<ul class='navbar-nav'>
+		// 				<li class='nav-item active'>
+		// 					<a class='nav-link' href='#'>
+		// 						Home <span class='sr-only'>(current)</span>
+		// 					</a>
+		// 				</li>
+		// 				<li class='nav-item'>
+		// 					<a class='nav-link' href='#'>
+		// 						Features
+		// 					</a>
+		// 				</li>
+		// 				<li class='nav-item'>
+		// 					<a class='nav-link' href='#'>
+		// 						Pricing
+		// 					</a>
+		// 				</li>
+		// 				<li class='nav-item'>
+		// 					<a class='nav-link disabled' href='#'>
+		// 						Disabled
+		// 					</a>
+		// 				</li>
+		// 			</ul>
+		// 		</div>
+		// 	</nav>
+		// </div>
+		<div>
 			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
 				<NavLink className='navbar-brand' to='/'>
 					<a className='navbar-brand' href='#'>
@@ -14,10 +57,10 @@ export default function Navbar() {
 					</a>
 				</NavLink>
 				<button
-					className='navbar-toggler'
+					class='navbar-toggler'
 					type='button'
-					data-toggle='collapse'
-					data-target='#navbarNav'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarNav'
 					aria-controls='navbarNav'
 					aria-expanded='false'
 					aria-label='Toggle navigation'
